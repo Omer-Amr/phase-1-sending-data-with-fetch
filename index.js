1cd6df0f3825bd093d2c7774b2ca6b21c562e71a
omer <you@example.com>
2022-12-15 17:18:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email){
+    const configUser = {
+        method : "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name : name,
+            email : email
+        })
+    }
+
+   return fetch("http://localhost:3000/users", configUser)
+    .then((res) => res.json())
+    .then((obj) => {
+        let idParagraph = document.createElement('p')
+        idParagraph.innerHTML = obj.id
+        document.querySelector("body").appendChild(idParagraph)
+    })
+    .catch(function(err){
+        let errParagraph = document.createElement('p');
+        errParagraph.textContent = err.message;
+        document.querySelector("body").appendChild(errParagraph)
+    })
+   
+}
